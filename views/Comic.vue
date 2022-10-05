6990d9c6478739d2c962433a2e5296ef54fed237
Igor Lira <igorlirap@gmail.com>
2022-10-05 00:57:49 -0300

    update comic page style

--- views/Comic.vue
+++ views/Comic.vue
@@ -1,6 +1,6 @@
 <template>
-  <div v-if="comicData.data">
-    <h1>{{ title }}</h1>
+  <div v-if="comicData.data" class="wrapper">
+    <div class="comic-title">{{ title }}</div>
     <img class="comic-image" :src="imageURL" width="300" height="400" />
     <div class="comic-description">{{ description }}</div>
     <div v-if="comicData.data.prices" class="comic-price">
@@ -24,14 +24,15 @@ export default {
     const request = new Request(route.query.url?.toString());
 
     request.searchInApi((apiData: any) => {
-      console.log(apiData);
       comicData.data = apiData[0];
       getStory(comicData.data.stories.items);
     });
 
     const title = computed(() => comicData.data.title);
     const description = computed(() => {
-        return comicData.data.description ? comicData.data.description: 'Story not available';
+      return comicData.data.description
+        ? comicData.data.description
+        : "Story not available";
     });
     const imageURL = computed(
       () =>
@@ -47,28 +48,28 @@ export default {
         innerStory.value = apiData.description;
       });
     };
-    const printprice = computed (() => {
-        let price = '';
-        for (let i = 0; i < comicData.data.prices.length; i++){
-            console.log (comicData.data.prices[i]);
-            if (comicData.data.prices[i].type == 'printPrice'){
-                price = `Print Price: ${comicData.data.prices[i].price} $`
-            }
+    const printprice = computed(() => {
+      let price = "";
+      for (let i = 0; i < comicData.data.prices.length; i++) {
+        if (comicData.data.prices[i].type == "printPrice") {
+          price = `Print Price: ${comicData.data.prices[i].price} $`;
         }
-        price = price === 'Print Price: 0 $' ? 'Print Price: Not Available' : price;
-        return price;
-    })
-    const digitalPrice = computed (() => {
-        let price = '';
-        for (let i = 0; i < comicData.data.prices.length; i++){
-            console.log (comicData.data.prices[i]);
-            if (comicData.data.prices[i].type == 'digitalPurchasePrice'){
-                price = `Digital Price: ${comicData.data.prices[i].price} $`
-            }
+      }
+      price =
+        price === "Print Price: 0 $" ? "Print Price: Not Available" : price;
+      return price;
+    });
+    const digitalPrice = computed(() => {
+      let price = "";
+      for (let i = 0; i < comicData.data.prices.length; i++) {
+        if (comicData.data.prices[i].type == "digitalPurchasePrice") {
+          price = `Digital Price: ${comicData.data.prices[i].price} $`;
         }
-        price = price === 'Digital Price: 0 $' ? 'Digital Price: Not Available' : price;
-        return price;
-    })
+      }
+      price =
+        price === "Digital Price: 0 $" ? "Digital Price: Not Available" : price;
+      return price;
+    });
 
     return {
       comicData,
@@ -85,28 +86,39 @@ export default {
 </script>
 
 <style scoped>
+* {
+  color: white;
+}
+.comic-title {
+  margin-top: 10px;
+  width: 100%;
+  text-align: center;
+  font: 400 32px/1.1 RobotoCondensed Bold, Trebuchet MS, Helvetica, Arial,
+    sans-serif;
+}
+.comic-description,
+.comic-price {
+  text-align: center;
+  font: 400 16px/1.5 Open Sans, Trebuchet MS, Helvetica, Arial, sans-serif;
+}
+
+.wrapper {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
 .comic-image {
   display: inline-block;
   padding: 10px;
 }
 .comic-description {
-  display: inline-block;
-  vertical-align: top;
-  white-space: normal;
-  margin-top: 50px;
-  overflow: hidden;
-  width: 30%;
-  height: 400px;
-  padding: 10px;
+  margin-top: 20px;
+  width: 100%;
 }
 .comic-price {
-  font-size: 20px;
-  white-space: normal;
-  display: inline-block;
-  vertical-align: top;
-  margin-top: 50px;
-  width: 30%;
-  height: 300px;
-  padding: 30px;
+  margin-top: 20px;
+  width: 100%;
 }
-</style>>
+</style>
+>
